2abfdcd48dc5a580ef4b217b1fc46a1e17a912c2
Alpfury <101234721+Alphurion@users.noreply.github.com>
2026-05-08 15:28:15 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <vector>
+
+
+/*
+  
+
+*/
+
+void dfs()
+{
+
+}
+
+int main()
+{
+  int v;
+  
+  std::string location, destination;
+  std::unordered_map<std::string, std::string> predecessor;
+  std::unordered_map<std::string, int> ammo_used;
+  std::unordered_map<std::string, int> enemies_at_location;
+  
+  return 0;
+}
